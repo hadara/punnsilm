9fe76803eebd4f2ec56f7e318bbd39f099934440
Sven Petai <hadara@bsd.ee>
2014-04-25 12:40:34 +0300

    added fbsd syslogd logsample + example artificial ignorance configuration

---
+++ test/simple_ignorance.py
@@ -0,0 +1,40 @@
+SSHD_TAG = "^sshd\[\d+\]: "
+
+NODE_LIST = [
+    {
+        'name': 'syslog_source',
+        'type': 'syslog_file_monitor',
+        'outputs': ['filter',],
+        'params': {
+            'filename': 'auth.log',
+        }
+    },
+    {
+        # ignore known good sshd messages
+        'name': 'filter',
+        'type': 'rx_grouper',
+        'params': {
+            'groups': {
+                'ignore': {
+                    'rx_list': [
+                        SSHD_TAG + "Accepted password for",
+                        SSHD_TAG + "pam_unix\(sshd:session\): session opened for",
+                        SSHD_TAG + "Received disconnect from ",
+                        SSHD_TAG + "pam_unix\(sshd:session\): session closed for user",
+                        SSHD_TAG + "Connection closed by",
+                    ],
+                    'outputs': [],
+                },
+                '_fallthrough': {
+                    'outputs': ['console'],
+                },
+            },
+        },
+    },
+    {
+        'name': 'console',
+        'type': 'console_output',
+        'outputs': [],
+    },
+]
+
